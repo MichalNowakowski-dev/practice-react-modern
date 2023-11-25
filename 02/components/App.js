9ec1b25902dcfa1d242fe6b502351a848e9b1c4a
mikroTesting <nowakowski.m1995@gmail.com>
2023-11-25 11:25:01 +0100

    task02 done

--- 02/components/App.js
+++ 02/components/App.js
@@ -1,16 +1,21 @@
 // ./src/components/App.js
 import React from 'react';
 import Box from './Box';
+import { TitleContext } from '../context';
 
 class App extends React.Component {
     state = {
-        text: 'React HelloWorld Modern!',
+        text: 'React HelloWorld !',
     };
 
     render() {
         const { text } = this.state;
 
-        return <Box text={text} />;
+        return (
+            <TitleContext.Provider value={text}>
+                <Box />
+            </TitleContext.Provider>
+        );
     }
 }
 
